b0250085335204b20dc2debe09370ada8cb8153a
YasinSadman <sadmanyasin007@gmail.com>
2026-05-13 00:41:34 +0600

    Program that prints digits from 1 to n.

---
+++ printn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // This prints the text to the screen
+    int n;
+    printf("Enter the vaalue of n...:");
+    scanf("%d",&n);
+    for(int i = 1;i <= n;i++){
+        printf("%d\n",i);
+    }
+    
+    return 0;
+} 
