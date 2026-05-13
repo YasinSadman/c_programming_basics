92e70f8e2c51d4a75ab59352c9455f2ccc83cccd
YasinSadman <sadmanyasin007@gmail.com>
2026-05-13 10:32:20 +0600

    Add a program to calculate soln of two linear eqnn

---
+++ twolineareqn.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    
+    double a1,a2,b1,b2,c1,c2;
+
+    printf("Enter the value of a1...:");
+    scanf("%lf",&a1);
+    printf("Enter the value of b1...:");
+    scanf("%lf",&b1);
+    printf("Enter the value of c1...:");
+    scanf("%lf",&c1);
+    printf("Enter the value of a2...:");
+    scanf("%lf",&a2);
+    printf("Enter the value of b2...:");
+    scanf("%lf",&b2);
+    printf("Enter the value of c2...:");
+    scanf("%lf",&c2);
+    
+if(((a1*b2)-(a2*b1)) == 0.00){
+    printf("Invalid Input..");
+}
+else {
+    printf("The required value of x is : %0.3lf\n",((c1*b2)-(c2*b1))/((a1*b2)-(a2*b1)));
+    printf("The required value of y is : %0.3lf\n",((a1*c2)-(a2*c1))/((a1*b2)-(a2*b1)));
+}
+    
+
+    
+    return 0;
+}
