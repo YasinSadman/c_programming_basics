d1d4aa8400568cafaca3c9fbb822859000507b38
YasinSadman <sadmanyasin007@gmail.com>
2026-05-13 12:59:11 +0600

    Add a program to find mass from pressure,volume & temperature

---
+++ masscalculation.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    double P,V,T,m;
+
+    printf("Enter the amount of Pressure...:");
+    scanf("%lf",&P);
+    printf("Enter the amount of Volume...:");
+    scanf("%lf",&V);
+    printf("Enter the amount of Temperature...:");
+    scanf("%lf",&T);
+
+    m =((P*V)/(0.37*(T+460)));
+    printf("The mass is....:%0.3lf\n",m);
+    
+    
+    return 0;
+}
