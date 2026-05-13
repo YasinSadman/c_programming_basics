69580733c65d294cfa09b7274b4faf6fe5cbc13a
YasinSadman <77431033+YasinSadman@users.noreply.github.com>
2026-05-13 10:43:14 +0600

    Refactor determinant calculation for linear equations

--- twolineareqn.c
+++ twolineareqn.c
@@ -2,7 +2,7 @@
 
 int main() {
     
-    double a1,a2,b1,b2,c1,c2;
+    double a1,a2,b1,b2,c1,c2,D;
 
     printf("Enter the value of a1...:");
     scanf("%lf",&a1);
@@ -16,13 +16,14 @@ int main() {
     scanf("%lf",&b2);
     printf("Enter the value of c2...:");
     scanf("%lf",&c2);
+    D = ((a1*b2)-(a2*b1));
     
-if(((a1*b2)-(a2*b1)) == 0.00){
+if(D == 0.00){
     printf("Invalid Input..");
 }
 else {
-    printf("The required value of x is : %0.3lf\n",((c1*b2)-(c2*b1))/((a1*b2)-(a2*b1)));
-    printf("The required value of y is : %0.3lf\n",((a1*c2)-(a2*c1))/((a1*b2)-(a2*b1)));
+    printf("The required value of x is : %0.3lf\n",((c1*b2)-(c2*b1))/D);
+    printf("The required value of y is : %0.3lf\n",((a1*c2)-(a2*c1))/D);
 }
     
 
