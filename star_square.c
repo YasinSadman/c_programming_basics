daf39ae45ee6a796a0f4822095df81e029d051eb
YasinSadman <sadmanyasin007@gmail.com>
2026-06-13 18:23:29 +0600

    Add a program to print 5x5 grid stars

---
+++ star_square.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    for(int i = 0;i < 5;i++){ 
+        for(int m = 0;m < 5;m++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
