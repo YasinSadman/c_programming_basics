a69319dcba14205e0b61dadd7e9b303926cc3858
YasinSadman <77431033+YasinSadman@users.noreply.github.com>
2026-08-21 18:55:10 +0600

    Fix sum calculation for odd series in practice_task_1.c

--- practice_task_1.c
+++ practice_task_1.c
@@ -5,12 +5,12 @@ int main() {
     printf("The question is we have to print the sum of the series 1+3+5+...+n\nIt can be seen this is an odd series.\n");
     printf("Enter a number :");
 
-    int num,m=1;
+    int num,m=0;
 
     scanf("%d",&num);
 
-    for(int i = 1,n=3;i <= num;i=i+2,n=n+2){
-        m = m + n;
+    for(int i = 1;i <= num;i = i + 2){
+        m = m + i;
     }
 
     printf("%d\n",m);
